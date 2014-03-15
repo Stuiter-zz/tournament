8d0f025ddc79434c0be3bc6725fba732efafb926
Stuiter <tim@kotterlink.nl>
2014-03-15 20:37:06 +0100

    Created the Java docs and added a print statement for the ArrayList.

--- src/nl/kotterlink/jsc/JacStammesCup.java
+++ src/nl/kotterlink/jsc/JacStammesCup.java
@@ -18,7 +18,7 @@ public class JacStammesCup {
 	public static void main(String[] args) {
 
 		//Creating the ArrayList for the object team.
-		List<Team> list = new ArrayList<Team>();
+		List<Team> listPAGD = new ArrayList<Team>();
 
 		//PAGD stands for Poule A Gemengd D.
 		Team teamPAGD1 = new Team("FIQAS Aalsmeer JD");
@@ -26,7 +26,17 @@ public class JacStammesCup {
 		Team teamPAGD3 = new Team("Houten");
 		Team teamPAGD4 = new Team("Gemini");
 
+		//Adding the teams to the listPAGD.
+		listPAGD.add(teamPAGD1);
+		listPAGD.add(teamPAGD2);
+		listPAGD.add(teamPAGD3);
+		listPAGD.add(teamPAGD4);
+
+		//show the listPAGD items in the console output.
+		for (Team team : listPAGD) {
+			System.out.println(team);
+		}
+
 		
 	}
-
 }
