4bc5a48ca6d50de7d2a1556f45578905726a37a1
hmmlopez <hmmlopez@outlook.com>
2014-04-15 23:15:42 +0200

    Make better use of th the grouping everything to a List.

--- src/nl/kotterlink/jsc/Poule.java
+++ src/nl/kotterlink/jsc/Poule.java
@@ -24,7 +24,7 @@ public class Poule {
 	public String toString() {
 		return "Poule [pouleName=" + pouleName + ", poule=" + poule + "]";
 	}
-	
+
 	public void printPoule() {
 		System.out.println("Poule Name: " + pouleName);
 		for (Team team : poule) {
@@ -35,71 +35,52 @@ public class Poule {
 
 	public void numberOfGames() {
 		int numberOfTeams = poule.size();
-		int iteration = 0;
-		String game1 = "";
-		String game2 = "";
-		String game3 = "";
-		String game4 = "";
-		String game5 = "";
-		String game6 = "";
-		String game7 = "";
-		String game8 = "";
-		String game9 = "";
-		String game10 = "";
+		List<String> games = new ArrayList<String>();
 		switch (numberOfTeams) {
-			//total number of games is 1
-			case 2:		game1 = poule.get(0).getTeam() + " - " + poule.get(1).getTeam();
-						iteration = 1;
-						break;
-						//total number of games is 3
-			case 3:		game1 = poule.get(0).getTeam() + " - " + poule.get(1).getTeam();
-						game2 = poule.get(0).getTeam() + " - " + poule.get(2).getTeam();
-						game3 = poule.get(1).getTeam() + " - " + poule.get(2).getTeam();
-						iteration = 3;
-						break;
-						//total number of games is 6
-			case 4:		game1 = poule.get(0).getTeam() + " - " + poule.get(1).getTeam();
-						game2 = poule.get(2).getTeam() + " - " + poule.get(3).getTeam();
-						game3 = poule.get(0).getTeam() + " - " + poule.get(2).getTeam();
-						game4 = poule.get(1).getTeam() + " - " + poule.get(3).getTeam();
-						game5 = poule.get(0).getTeam() + " - " + poule.get(3).getTeam();
-						game6 = poule.get(1).getTeam() + " - " + poule.get(2).getTeam();
-						iteration = 6;
-						break;
-						//total number of games is 10
-			case 5:		game1 = poule.get(0).getTeam() + " - " + poule.get(1).getTeam();
-						game2 = poule.get(0).getTeam() + " - " + poule.get(2).getTeam();
-						game3 = poule.get(0).getTeam() + " - " + poule.get(3).getTeam();
-						game4 = poule.get(0).getTeam() + " - " + poule.get(4).getTeam();
-						game5 = poule.get(1).getTeam() + " - " + poule.get(2).getTeam();
-						game6 = poule.get(1).getTeam() + " - " + poule.get(3).getTeam();
-						game7 = poule.get(1).getTeam() + " - " + poule.get(4).getTeam();
-						game8 = poule.get(2).getTeam() + " - " + poule.get(3).getTeam();
-						game9 = poule.get(2).getTeam() + " - " + poule.get(4).getTeam();
-						game10 = poule.get(3).getTeam() + " - " + poule.get(4).getTeam();
-						iteration = 10;
-						break;
-						//total number of games is 0
-			default:	
-						break;
+		// total number of games is 1
+		case 2:
+			games.add(poule.get(0).getTeam() + " - " + poule.get(1).getTeam());
+			break;
+		// total number of games is 3
+		case 3:
+			games.add(poule.get(0).getTeam() + " - " + poule.get(1).getTeam());
+			games.add(poule.get(0).getTeam() + " - " + poule.get(2).getTeam());
+			games.add(poule.get(1).getTeam() + " - " + poule.get(2).getTeam());
+			break;
+		// total number of games is 6
+		case 4:
+			games.add(poule.get(0).getTeam() + " - " + poule.get(1).getTeam());
+			games.add(poule.get(0).getTeam() + " - " + poule.get(2).getTeam());
+			games.add(poule.get(0).getTeam() + " - " + poule.get(3).getTeam());
+			games.add(poule.get(1).getTeam() + " - " + poule.get(2).getTeam());
+			games.add(poule.get(1).getTeam() + " - " + poule.get(3).getTeam());
+			games.add(poule.get(2).getTeam() + " - " + poule.get(3).getTeam());
+			break;
+		// total number of games is 10
+		case 5:
+			games.add(poule.get(0).getTeam() + " - " + poule.get(1).getTeam());
+			games.add(poule.get(0).getTeam() + " - " + poule.get(2).getTeam());
+			games.add(poule.get(0).getTeam() + " - " + poule.get(3).getTeam());
+			games.add(poule.get(0).getTeam() + " - " + poule.get(4).getTeam());
+			games.add(poule.get(1).getTeam() + " - " + poule.get(2).getTeam());
+			games.add(poule.get(1).getTeam() + " - " + poule.get(3).getTeam());
+			games.add(poule.get(1).getTeam() + " - " + poule.get(4).getTeam());
+			games.add(poule.get(2).getTeam() + " - " + poule.get(3).getTeam());
+			games.add(poule.get(2).getTeam() + " - " + poule.get(4).getTeam());
+			games.add(poule.get(3).getTeam() + " - " + poule.get(4).getTeam());
+			break;
+		// total number of games is 0
+		default:
+			break;
 		}
-	
-//		for (int i = 0; i < iteration; i++) {
-//			System.out.println(game + i);
-//		}
 
 		System.out.println("This is poule: " + Poule.this.getPouleName());
-		System.out.println("------------------------------------------------------------------------");
-		System.out.println(game1 + "\n"
-							+ game2 + "\n"
-							+ game3 + "\n"
-							+ game4 + "\n"
-							+ game5 + "\n"
-							+ game6 + "\n"
-							+ game7 + "\n"
-							+ game8 + "\n"
-							+ game9 + "\n"
-							+ game10 + "\n"
-				);
+		System.out
+				.println("------------------------------------------------------------------------");
+		for (int i = 0; i < games.size(); i++) {
+			System.out.println(games.get(i) + i);
+		}
+		System.out.println();
+
 	}
 }
